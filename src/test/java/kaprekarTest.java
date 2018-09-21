a6c2c01665a6401d8ecd1b6da11da92243bbaf92
rasool <rasools27@gmail.com>
2018-09-21 06:56:43 +0530

    this project netbeans3

---
+++ src/test/java/kaprekarTest.java
@@ -0,0 +1,25 @@
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+public class kaprekarTest {
+    
+    public kaprekarTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+}
